6fc6f30077e8935c0a81df0d19a2160a65f01c9d
Mansur Besleney <besleneymansur@gmail.com>
2024-10-03 17:04:38 +0300

    static files enabled, bootstrap added

--- LawOfficeManagmentWebApp/Program.cs
+++ LawOfficeManagmentWebApp/Program.cs
@@ -4,7 +4,11 @@ builder.Services.AddControllersWithViews();
 
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"); // mapping of the url .../Home/Index
