f9b2770ec9ed6b893643d157b1be80496136fdd9
Mansur Besleney <besleneymansur@gmail.com>
2024-10-07 22:40:25 +0300

    initial migration added, connection stuff done

---
+++ LawOfficeManagmentWebApp/Migrations/20241007193423_InitialMigraiton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LawOfficeManagmentWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigraiton : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientTcNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ClientSurname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ClientGSM = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
--- LawOfficeManagmentWebApp/Program.cs
+++ LawOfficeManagmentWebApp/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LawOfficePortal")));
 
 var app = builder.Build();
 
