0a655a3b8cb1ea10de7c9427b1b50665f9277b9b
Mansur Besleney <besleneymansur@gmail.com>
2024-10-09 17:04:28 +0300

    IClientRepository and ClientRepository added

---
+++ LawOfficeManagmentWebApp/Repositories/ClientRepository.cs
@@ -0,0 +1,44 @@
+﻿using LawOfficeManagmentWebApp.Data;
+using LawOfficeManagmentWebApp.Models.Entites;
+using LawOfficeManagmentWebApp.Repostories;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LawOfficeManagmentWebApp.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        public async Task<Client?> GetById(int id)
+        {
+            Client? client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                throw new KeyNotFoundException($"Client with ID {id} not found.");
+            }
+
+            return client;
+            
+        }
+
+        public async Task CreateAsync(Client client)
+        {
+            _context.Clients.Add(client);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ LawOfficeManagmentWebApp/Repositories/IClientRepository.cs
@@ -0,0 +1,11 @@
+﻿using LawOfficeManagmentWebApp.Models.Entites;
+
+namespace LawOfficeManagmentWebApp.Repostories
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAllAsync();
+        Task<Client?> GetById(int id);
+        Task CreateAsync(Client client);
+    }
+}
