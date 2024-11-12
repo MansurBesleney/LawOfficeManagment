2464a5d9e9f08eef4bcca3ec641c2a1fc7d58766
Mansur Besleney <besleneymansur@gmail.com>
2024-11-12 11:49:25 +0300

    ILawyerService and LawyerService added

---
+++ LawOfficeManagementWebApp/Services/ILawyerService.cs
@@ -0,0 +1,14 @@
+﻿
+using LawOfficeManagementWebApp.Models.Dtos;
+
+namespace LawOfficeManagementWebApp.Services
+{
+    public interface ILawyerService
+    {
+        Task<IEnumerable<LawyerDto>> GetAllLawyersAsync();
+        Task<LawyerDto> GetLawyerByIdAsync(Guid id);
+        Task CreateLawyerAsync(LawyerDto lawyerDto);
+        Task UpdateLawyerAsync(LawyerDto lawyerDto);
+        Task DeleteLawyerAsync(Guid id);
+    }
+}
---
+++ LawOfficeManagementWebApp/Services/LawyerService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using LawOfficeManagementWebApp.Models.Dtos;
+using LawOfficeManagementWebApp.Models.Entites;
+using LawOfficeManagementWebApp.Repositories;
+
+namespace LawOfficeManagementWebApp.Services
+{
+    public class LawyerService : ILawyerService
+    {
+        private readonly ILawyerRepository _lawyerRepository;
+        private readonly IMapper _mapper;
+
+        public LawyerService(ILawyerRepository lawyerRepository, IMapper mapper)
+        {
+            _lawyerRepository = lawyerRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<LawyerDto>> GetAllLawyersAsync()
+        {
+            var lawyers = await _lawyerRepository.GetAllAsync();
+
+            return _mapper.Map<IEnumerable<LawyerDto>>(lawyers);
+        }
+
+        public async Task<LawyerDto> GetLawyerByIdAsync(Guid id)
+        {
+            var lawyer = await _lawyerRepository.GetByIdAsync(id);
+
+            return _mapper.Map<LawyerDto>(lawyer);
+        }
+
+        public async Task CreateLawyerAsync(LawyerDto lawyerDto)
+        {
+            var lawyer = _mapper.Map<Lawyer>(lawyerDto);
+
+            await _lawyerRepository.CreateAsync(lawyer);
+        }
+
+        public async Task UpdateLawyerAsync(LawyerDto lawyerDto)
+        {
+            var lawyer = _mapper.Map<Lawyer>(lawyerDto);
+
+            await _lawyerRepository.UpdateAsync(lawyer);
+        }
+
+        public async Task DeleteLawyerAsync(Guid id)
+        {
+            await _lawyerRepository.DeleteAsync(id);
+        }
+    }
+}
